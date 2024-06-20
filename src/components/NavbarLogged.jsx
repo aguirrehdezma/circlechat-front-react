03ba1fcd87bf92a859586408c2053e348f1d596c
aguirrehdezma <aguirrehdezma@gmail.com>
2024-06-19 22:04:28 -0700

    Add Sign Out feature

--- src/components/NavbarLogged.jsx
+++ src/components/NavbarLogged.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import styles from "../styles/NavbarLogged.module.css";
+import api from "../api";
+import { useAuth } from "../contexts/AuthContext";
 
 function NavbarLogged() {
     const [isActive, setIsActive] = useState(false);
+    const { removeToken } = useAuth();
+
     const toggleActiveClass = () => {
         setIsActive(!isActive);
     };
     const removeActive = () => {
         setIsActive(false);
     };
+
+    const signOut = async () => {
+        await api
+            .post('/api/v1/token/logout/')
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(JSON.stringify(error));
+            });
+
+        api.defaults.headers.common["Authorization"] = "";
+        removeToken();
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -29,6 +48,13 @@ function NavbarLogged() {
                         <li onClick={removeActive}>
                             <a href="/faq" className={`${styles.navLink}`}>
                                 FAQ
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className={`${styles.navSignOut}`}>
+                        <li onClick={signOut}>
+                            <a href="#" className={`${styles.navLink}`}>
+                                SIGN OUT
                             </a>
                         </li>
                     </ul>
